09e808174c1a3ad868fb5cdffacd57b7f5d123ad
walduino <walduino@users.noreply.github.com>
2015-12-01 23:20:04 +0100

    remove debug

--- DhtToMqtt.ino
+++ DhtToMqtt.ino
@@ -1,4 +1,3 @@
-#include <SoftwareSerial.h>
 #include <espduino.h>
 #include <mqtt.h>
 #include <dht.h>
@@ -9,10 +8,10 @@
 #define MQTTCLIENT "Cheapspark"
 #define MQTTTOPIC0 "/cheapspark/humi"
 #define MQTTTOPIC1 "/cheapspark/temp"
-#define DHT44_PIN 12
+#define DHT_PIN 5
 
-SoftwareSerial debugPort(2, 3); // RX, TX
-ESP esp(&Serial, &debugPort, 4);
+
+ESP esp(&Serial, 4);
 MQTT mqtt(&esp);
 boolean wifiConnected = false;
 int reportInterval =  15000;
@@ -29,8 +28,7 @@ void wifiCb(void* response)
 
   if(res.getArgc() == 1) {
     res.popArgs((uint8_t*)&status, 4);
-    if(status == STATION_GOT_IP) {
-      debugPort.println("WIFI CONNECTED");
+    if(status == STATION_GOT_IP) {        //WIFI CONNECTED
       mqtt.connect(BROKERIP, 1883, false);
       wifiConnected = true;
     } else {
@@ -43,7 +41,6 @@ void wifiCb(void* response)
 
 void mqttConnected(void* response)
 {
-  debugPort.println("Connected");
   mqtt.publish(MQTTTOPIC0, "data0");
   mqtt.publish(MQTTTOPIC1, "data1");
 }
@@ -55,61 +52,52 @@ void mqttData(void* response)
 {
   RESPONSE res(response);
 
-  debugPort.print("Received: topic=");
+//db  debugPort.print("Received: topic=");
   String topic = res.popString();
-  debugPort.println(topic);
+//db  debugPort.println(topic);
 
-  debugPort.print("data=");
+//db  debugPort.print("data=");
   String data = res.popString();
-  debugPort.println(data);
+//db  debugPort.println(data);
 
 }
 void mqttPublished(void* response)
 {
-  debugPort.println("Published: Succes");
+//runs when publish is a success
 }
 void setup() {
   delay(5000);
   Serial.begin(19200);
-  debugPort.begin(19200);
   esp.enable();
   delay(500);
   esp.reset();
   delay(500);
   while(!esp.ready());
 
-  debugPort.println("ARDUINO: setup mqtt client");
+//setup mqtt client");
   if(!mqtt.begin(MQTTCLIENT, "", "", 120, 1)) {
-    debugPort.println("ARDUINO: fail to setup mqtt");
     while(1);
   }
 
-  debugPort.println("ARDUINO: setup mqtt lwt");
+//setup mqtt lwt
   mqtt.lwt("/lwt", "offline", 0, 0);
   
   /*setup wifi*/
-  debugPort.println("ARDUINO: setup wifi");
   esp.wifiCb.attach(&wifiCb);
-
   esp.wifiConnect(MYSSID,MYPASS);
 
-  debugPort.println("ARDUINO: system started");
 }
 
 void loop() {
   esp.process();
-  
   digitalWrite(ledpin, ledstate);
-  
   if(wifiConnected) {
-    
     now = millis();
-    
-    if ((digitalRead(3) == HIGH) && now >= nextPub) { //alarm sensors are NC if using a switch make this LOW
+    if (now >= nextPub) {
       
       nextPub = reportInterval + now;
       
-      int chk = DHT.read44(12);
+      int chk = DHT.read22(DHT_PIN);
       float humid = DHT.humidity;
       float tempe = DHT.temperature;
       
